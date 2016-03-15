20fc3aa3be4fac5f84ab76638bdbddd1848cd88c
elmkarami <karami.elmehdi@gmail.com>
2016-03-15 13:23:02 +0000

    add status_code + message to RequestError

--- contextio/__init__.py
+++ contextio/__init__.py
@@ -134,6 +134,13 @@ class CXAPIException(Exception):
 class RequestError(CXAPIException):
     """Class to handle request errors."""
 
+    def __init__(self, status_code, message):
+        self.status_code = status_code
+        self.message = message
+
+    def __str__(self):
+        return 'HTTP {}: {}'.format(self.status_code, self.message)
+
 
 class ArgumentError(CXAPIException):
     """Class to handle bad arguments."""
